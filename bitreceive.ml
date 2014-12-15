6e993244ce7764a83f22a98329af82ad9354d459
Pedro Oliva <pedraolivo@gmail.com>
2014-12-15 15:23:37 +0700

    moved mutext_file to commit and fixed a bug on walletnotify on blockhash is unavailable until nconfirm > 0

--- bitreceive.ml
+++ bitreceive.ml
@@ -3,7 +3,7 @@ open Core.Std
 open Bitcoin
 
 type config_t = {
-  rpcip: string; rpcport: int; rpcuser: string; rpcpassword: string; nconfirm: int; state_file: string;
+  rpcip: string; rpcport: int; rpcuser: string; rpcpassword: string; nconfirm: int; mutex_file:string; state_file: string;
   min_seconds_between_call: int; output_pipe: string; 
 } with sexp
 
@@ -16,8 +16,8 @@ end
 
 module CoinService = Bitcoin.Make (Bitcoin_ocamlnet.Httpclient) (Connection_config)
 
-let mutex () =
-  let mutex_file = "/tmp/bitreceive.lock" in
+let mutex config =
+  let mutex_file = config.mutex_file in
   let fd = Unix.openfile mutex_file ~mode:[Unix.O_WRONLY;Unix.O_CREAT] in
   let _ = Unix.lockf fd ~mode:Unix.F_LOCK ~len:(Int64.of_int 0) in
   fd
@@ -46,12 +46,12 @@ let scan_and_notify config state =
     | Some blockhash -> CoinService.listsinceblock ~conn ~blockhash ()
   in
   let (min_safe_block_info, has_work, txs) = List.fold_left ~init:(None,false,[]) ~f:(fun acc tx -> 
-      Yojson.Safe.pretty_to_channel stdout (`Assoc tx);
+      (*Yojson.Safe.pretty_to_channel stdout (`Assoc tx);*)
       let (min_safe_block_info,has_work,tx_acc) = acc in
       match (List.Assoc.find tx "category") with
         Some (`String "receive") -> 
-        begin match (List.Assoc.find tx "address", List.Assoc.find tx "amount", List.Assoc.find tx "blockhash", List.Assoc.find tx "confirmations") with
-            (Some (`String address), Some (`Float amount), Some (`String blockhash), Some (`Int confirmations)) -> 
+        begin match (List.Assoc.find tx "address", List.Assoc.find tx "amount", List.Assoc.find tx "confirmations") with
+            (Some (`String address), Some (`Float amount), Some (`Int confirmations)) -> 
             let tx_acc = `Assoc [
               ("address", (`String address));
               ("amount", (`Intlit (Int64.to_string (Bitcoin.amount_of_float amount))));
@@ -59,13 +59,14 @@ let scan_and_notify config state =
             ] :: tx_acc in
             let min_safe_block_info =
               if confirmations >= config.nconfirm then
-                begin match min_safe_block_info with
-                    None -> Some (confirmations, blockhash)
-                  | Some (min_safe_block_nconfirm,min_safe_block) -> 
+                begin match (List.Assoc.find tx "blockhash", min_safe_block_info) with
+                    (Some (`String blockhash), None) -> Some (confirmations, blockhash)
+                  | (Some (`String blockhash), Some (min_safe_block_nconfirm,min_safe_block)) -> 
                     if min_safe_block_nconfirm > confirmations then
                       Some (confirmations, blockhash)
                     else
                       min_safe_block_info
+                  | _ -> assert false
                 end
               else
                 min_safe_block_info
@@ -78,19 +79,19 @@ let scan_and_notify config state =
   in
   let last_included_block =
     match min_safe_block_info with
-      None -> None
+      None -> state.last_included_block
     | Some (_, blockhash) -> Some blockhash
   in
 
   let json_string = Yojson.Safe.to_string (`Assoc [("last_included_block", `String (match last_included_block with None -> "" | Some bl -> bl));("incoming",`List txs)]) in
-  Out_channel.write_all config.output_pipe ~data:json_string; 
+  Out_channel.write_all config.output_pipe ~data:(sprintf "%s\n" json_string);
   save_state config.state_file {last_included_block = last_included_block; has_work = has_work; last_called = Unix.time ()}
 
 let _ =
   match Sys.argv with
     [|_;"walletnotify";config_file;txid|] -> 
-    let mud = mutex () in
     let config = config_t_of_sexp (Sexp.of_string (In_channel.read_all config_file)) in
+    let mud = mutex config in
 
     let state = get_state config.state_file in
 
@@ -101,8 +102,8 @@ let _ =
 
     Unix.close mud
   | [|_;"blocknotify";config_file;blockid|] ->
-    let mud = mutex () in
     let config = config_t_of_sexp (Sexp.of_string (In_channel.read_all config_file)) in
+    let mud = mutex config in
 
     let state = get_state config.state_file in
     begin
