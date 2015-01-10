e0dab1f0a534019895cc26e69ec63f37ae6566f2
Pedro Oliva <pedraolivo@gmail.com>
2015-01-10 14:46:38 +0700

    previous commit does not work because json is tricky to parse, so here I am lumping it in the scan_notify function for now

--- bitreceive.ml
+++ bitreceive.ml
@@ -30,26 +30,26 @@ let get_state state_file =
 
 let save_state state_file state = Out_channel.write_all state_file ~data:(Sexp.to_string (sexp_of_state_t state))
 
-let is_receive_tx conn txid =
-  let tx =  CoinService.gettransaction ~conn txid in
-  match (List.Assoc.find tx "category") with
-    Some (`String "receive") -> true
-  | _ -> false
-
-let scan_and_notify config conn state =
+let scan_and_notify ?(notified_txid) config conn state =
   let (txs,_) =
     match state.last_included_block with
       None -> CoinService.listsinceblock ~conn ()
     | Some blockhash -> CoinService.listsinceblock ~conn ~blockhash ()
   in
-  let (min_safe_block_info, has_work, txs) = List.fold_left ~init:(None,false,[]) ~f:(fun acc tx -> 
+  let (is_notified_tx_receive,min_safe_block_info, has_work, txs) = List.fold_left ~init:(false,None,false,[]) ~f:(fun acc tx -> 
       (*Yojson.Safe.pretty_to_channel stdout (`Assoc tx);*)
 
-      let (old_min_safe_block_info,old_has_work,tx_acc) = acc in
+      let (is_notified_tx_receive,old_min_safe_block_info,old_has_work,tx_acc) = acc in
       match (List.Assoc.find tx "category") with
         Some (`String "receive") -> 
         begin match (List.Assoc.find tx "address", List.Assoc.find tx "amount", List.Assoc.find tx "confirmations", List.Assoc.find tx "txid") with
             (Some (`String address), Some (`Float amount), Some (`Int confirmations), Some (`String txid)) -> 
+            let is_it () =
+              match notified_txid with
+                None -> false
+              | Some notified_txid -> notified_txid = txid
+            in
+
             let tx_acc = `Assoc [
               ("address", (`String address));
               ("amount", (`Intlit (Int64.to_string (Bitcoin.amount_of_float amount))));
@@ -70,7 +70,7 @@ let scan_and_notify config conn state =
               else
                 old_min_safe_block_info
             in
-              (min_safe_block_info, (old_has_work || confirmations < config.nconfirm), tx_acc)
+              ((is_notified_tx_receive || is_it ()), min_safe_block_info, (old_has_work || confirmations < config.nconfirm), tx_acc)
           | _ -> assert false
         end
       | _ -> acc
@@ -82,9 +82,12 @@ let scan_and_notify config conn state =
     | Some (_, blockhash) -> Some blockhash
   in
 
-  let json_string = Yojson.Safe.to_string (`Assoc [("last_included_block", `String (match last_included_block with None -> "" | Some bl -> bl));("incoming",`List txs)]) in
-  Out_channel.write_all config.output_pipe ~data:(sprintf "%s\n" json_string);
-  save_state config.state_file {last_included_block = last_included_block; has_work = has_work; last_called = Unix.time ()}
+  if is_notified_tx_receive then
+    begin
+      let json_string = Yojson.Safe.to_string (`Assoc [("last_included_block", `String (match last_included_block with None -> "" | Some bl -> bl));("incoming",`List txs)]) in
+      Out_channel.write_all config.output_pipe ~data:(sprintf "%s\n" json_string);
+      save_state config.state_file {last_included_block = last_included_block; has_work = has_work; last_called = Unix.time ()}
+    end
 
 let _ =
   let get_conn config =
@@ -106,8 +109,7 @@ let _ =
 
     begin
     if (Unix.time() -. state.last_called > (Float.of_int config.min_seconds_between_call)) then
-      if is_receive_tx conn txid then
-        scan_and_notify config conn state
+      scan_and_notify ~notified_txid:(Some txid) config conn state
     end;
 
     Unix.close mud
